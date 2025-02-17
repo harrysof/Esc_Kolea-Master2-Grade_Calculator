36f4bc5a515662cde554697e50ae0ab2d6534ed6
Sofiane Belkacem Nacer <73562755+harrysof@users.noreply.github.com>
2025-02-17 19:36:12 +0100

    Update Master_2_Accounting_S1_CALC_GUI.py

--- Master_2_Accounting_S1_CALC_GUI.py
+++ Master_2_Accounting_S1_CALC_GUI.py
@@ -42,7 +42,7 @@ st.markdown("""
 
 st.markdown("""
     <div class="main-title">
-        Master 2 Accounting - S1 Grade Calculator<br>
+        Master 2 Accounting<br>S1 Grade Calculator<br>
         <span style="font-size: 1.2rem; color: #dcdcdc;">By Sofiane Belkacem Nacer</span>
     </div>
     """, unsafe_allow_html=True)
